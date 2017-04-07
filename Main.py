052be6dd37ada84a33214045d3583521bf3be2b2
kpugdeet <krittaphat.pug@gmail.com>
2017-04-07 16:20:18 -0400

    Update RBM TF

--- Main.py
+++ Main.py
@@ -2,10 +2,19 @@ import csv
 import numpy as np
 import pickle
 import os
+import sys
+from RbmTf import RBM
+import time
+
+np.set_printoptions(suppress=True)
 
 # User preference and its index
-userID = {}
+userID = dict()
 user = []
+
+# User preference ad its index for checking to predict
+userIDTarget = dict()
+userTarget = []
 
 # Read csv file
 if os.path.isfile("user.pkl") and os.path.isfile("userID.pkl"):
@@ -16,25 +25,82 @@ else:
         readCSV = csv.reader(csvFile, delimiter=',')
         next(readCSV, None)
         count = 0
-        # globalCount = 0
-        # prev = ""
-        # countEach = 0
+        countTarget = 0
         for i, row in enumerate(readCSV):
+            if row[0] >= sys.argv[1]:
+                checkID = row[1].strip()
+                if checkID in userID:
+                    index = userID[checkID]
+                    for _, x in enumerate(row[24:]):
+                        if user[index][_] == 0 and x != "NA":
+                            user[index][_] = x
+                else:
+                    userID[checkID] = count
+                    user.append(['0' if x == "NA" else x for x in row[24:]])
+                    count += 1
             if row[0] == "2016-05-28":
-                userID[row[1]] = count
-                user.append(['0' if x == 'NA' else x for x in row[24:]])
-                count += 1
-            # globalCount += 1
-            # if row[0] != prev:
-            #     print countEach
-            #     countEach = 0
-            #     print row[0]
-            #     prev = row[0]
-            # countEach += 1
-            # if i >= 999999:
-            #     break
-        user = np.array(user).astype(np.float)
-        pickle.dump(user, open("user.pkl", "wb"))
-        pickle.dump(userID, open("userID.pkl", "wb"))
+                checkID = row[1].strip()
+                userIDTarget[checkID] = countTarget
+                userTarget.append(['0' if x == "NA" else x for x in row[24:]])
+                countTarget += 1
 
+        user = np.array(user).astype(np.float)
+        userTarget = np.array(userTarget).astype(np.float)
+        # pickle.dump(user, open("user.pkl", "wb"))
+        # pickle.dump(userID, open("userID.pkl", "wb"))
+
+# Check the input Data is correct
 print (user.shape, len(userID))
+print (userTarget.shape, len(userIDTarget))
+label, indices = np.unique(user,return_inverse=True)
+count = np.bincount(indices)
+print (label, count)
+print (float(count[0]/float(count[1])))
+print (user.shape[0]*user.shape[1]) == np.sum(count)
+
+# Training RBM
+rbm = RBM (user.shape[1], user.shape[1]/2, ["w", "vb", "hb", "dW", "dVb", "dHb"], "./logs")
+startTime = time.time()
+rbm.fit(user, 100)
+rbm.saveWeights("./model.ckpt")
+# rbm.restoreWeights("./model.ckpt")
+print ("Time used: {0}".format(time.time()-startTime))
+
+# Read Test data
+test = []
+testID = []
+with open("./data/test_ver2.csv") as csvFile:
+    readCSV = csv.reader(csvFile, delimiter=',')
+    next(readCSV, None)
+    count = 0
+    for i, row in enumerate(readCSV):
+        test.append(userTarget[userIDTarget[row[1].strip()]])
+        testID.append(row[1].strip())
+
+# Do recommendation
+itemName = ["ind_ahor_fin_ult1", "ind_aval_fin_ult1", "ind_cco_fin_ult1", "ind_cder_fin_ult1", "ind_cno_fin_ult1", "ind_ctju_fin_ult1",
+            "ind_ctma_fin_ult1", "ind_ctop_fin_ult1", "ind_ctpp_fin_ult1", "ind_deco_fin_ult1", "ind_deme_fin_ult1", "ind_dela_fin_ult1",
+            "ind_ecue_fin_ult1", "ind_fond_fin_ult1", "ind_hip_fin_ult1", "ind_plan_fin_ult1", "ind_pres_fin_ult1", "ind_reca_fin_ult1",
+            "ind_tjcr_fin_ult1", "ind_valo_fin_ult1", "ind_viv_fin_ult1", "ind_nomina_ult1", "ind_nom_pens_ult1", "ind_recibo_ult1"]
+f = open('submission.csv', 'w')
+f.write("ncodpers,added_products\n")
+countEmpty = 0
+for _, pre in enumerate(rbm.predictV(np.array(test))):
+    userIndex = userIDTarget[testID[_]]
+    f.write("{0},".format(testID[_]))
+    sortPre = sorted(range(len(pre)), key=lambda k: pre[k], reverse=True)
+    outList = []
+    for index in sortPre:
+        if userTarget[userIndex][index] == 0 and pre[index] >= 0.01 and len(outList) < 7:
+            outList.append(itemName[index])
+    if len(outList) != 0:
+        f.write(" ".join(map(str, outList)))
+    else:
+        countEmpty += 1
+    f.write("\n")
+
+print countEmpty
+# target = 0
+# print rbm.predictH(np.array([user[target]]))
+# print rbm.predictV(np.array([user[target]]))
+# print user[target]
--- RbmTf.py
+++ RbmTf.py
@@ -1,13 +1,12 @@
 # Reference https://github.com/Cospel/rbm-ae-tf
 import os
-#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 import numpy as np
-import time
 
 class RBM:
-    def __init__(self, numVis, numHid, names, learningRate=0.5, weightDecay=0.0001, momentum=0.9):
-        with tf.device('/gpu:0'):
+    def __init__(self, numVis, numHid, names, path, typeRun="/cpu:0", learningRate=0.5, weightDecay=0.0001, momentum=0.9):
+        with tf.device(typeRun):
             self.numVis = numVis
             self.numHid = numHid
             self.names = names
@@ -18,14 +17,21 @@ class RBM:
             # Weight for save and restore Tensorflow
             self.weights = self.initialWeight()
 
-            # placeholders
+            # Placeholder and Variables
             self.x = tf.placeholder(tf.float32, [None, self.numVis])
             self.w = tf.Variable(tf.random_normal([self.numVis, self.numHid], mean=0.0, stddev=0.01, dtype=tf.float32))
-            self.dW = tf.Variable(tf.zeros([self.numVis, self.numHid], tf.float32))
             self.vb = tf.Variable(tf.zeros([self.numVis], tf.float32))
-            self.dVb = tf.Variable(tf.zeros([self.numVis], tf.float32))
             self.hb = tf.Variable(tf.zeros([self.numHid], tf.float32))
+
+            # For update
+            self.dW = tf.Variable(tf.zeros([self.numVis, self.numHid], tf.float32))
+            self.dVb = tf.Variable(tf.zeros([self.numVis], tf.float32))
             self.dHb = tf.Variable(tf.zeros([self.numHid], tf.float32))
+
+            # For save model
+            self.saveDW = tf.Variable(tf.zeros([self.numVis, self.numHid], tf.float32))
+            self.saveDVb = tf.Variable(tf.zeros([self.numVis], tf.float32))
+            self.saveDHb = tf.Variable(tf.zeros([self.numHid], tf.float32))
 
             # Initial Variables
             # The weights are initialized zero-mean Gaussian with a standard deviation of about 0.01.
@@ -59,37 +65,48 @@ class RBM:
 
             # Stochastic steepest ascent because we need to maximize log likelihood of p(visible)
             # dlog(p)/dlog(w) = (visible * hidden)_data - (visible * hidden)_reconstruction
-            # self.updateW = self.w + self.learningRate * (self.w_positive_grad - self.w_negative_grad) / tf.to_float(tf.shape(self.x)[0])
-            # self.updateVb = self.vb + self.learningRate * tf.reduce_mean(self.x - self.v1, 0)
-            # self.updateHb = self.hb + self.learningRate * tf.reduce_mean(self.h0prob - self.h1, 0)
-            self.dW = self.momentum * self.dW + self.learningRate * ((self.wPositiveGrad - self.wNegativeGrad) / tf.to_float(tf.shape(self.x)[0]) - self.weightDecay * self.w)
-            self.dVb = (self.learningRate * tf.reduce_mean(self.x - self.v1, 0)) + (self.momentum * self.dVb * self.learningRate / tf.to_float(tf.shape(self.x)[0]))
-            self.dHb = (self.learningRate * tf.reduce_mean(self.h0Prob - self.h1, 0)) + (self.momentum * self.dHb * self.learningRate / tf.to_float(tf.shape(self.x)[0]))
+            # self.dW = self.w + self.learningRate * (self.wPositiveGrad - self.wNegativeGrad) / tf.to_float(tf.shape(self.x)[0])
+            # self.dVb = self.vb + self.learningRate * tf.reduce_mean(self.x - self.v1, 0)
+            # self.dHb = self.hb + self.learningRate * tf.reduce_mean(self.h0Prob - self.h1, 0)
+            self.dW = self.momentum * self.saveDW + self.learningRate * ((self.wPositiveGrad - self.wNegativeGrad) / tf.to_float(tf.shape(self.x)[0]) - self.weightDecay * self.w)
+            self.dVb = (self.learningRate * tf.reduce_mean(self.x - self.v1, 0)) + (self.momentum * self.saveDVb * self.learningRate / tf.to_float(tf.shape(self.x)[0]))
+            self.dHb = (self.learningRate * tf.reduce_mean(self.h0Prob - self.h1, 0)) + (self.momentum * self.saveDHb * self.learningRate / tf.to_float(tf.shape(self.x)[0]))
 
             # Update weight
-            self.w += self.dW
-            self.vb += self.dVb
-            self.hb += self.dHb
+            self.updateAll = [self.w.assign_add(self.dW), self.vb.assign_add(self.dVb), self.hb.assign_add(self.dHb),
+                              self.saveDW.assign(self.dW), self.saveDVb.assign(self.dVb), self.saveDHb.assign(self.dHb)]
+            # self.w += self.dW
+            # self.vb += self.dVb
+            # self.hb += self.dHb
 
             # sampling functions
             self.hSample = tf.nn.sigmoid(tf.matmul(self.x, self.w) + self.hb)
             self.vSample = tf.nn.sigmoid(tf.matmul(self.hSample, tf.transpose(self.w)) + self.vb)
 
-            # cost
+            # Cost
             self.errSum = tf.reduce_mean(tf.square(self.x - self.vSample))
+            tf.summary.scalar("Error Sum", self.errSum)
 
             # Initial session
-            self.init = tf.initialize_all_variables()
+            self.init = tf.global_variables_initializer()
             # self.sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-            self.sess = tf.Session()
+            self.sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+            # self.sess = tf.Session()
             self.sess.run(self.init)
+
+            # For Tensorboard
+            self.merge = tf.summary.merge_all()
+            self.write = tf.summary.FileWriter(path, self.sess.graph)
 
     def initialWeight(self):
         # These weights are only for storing and loading model for Tensorflow Saver.
         allWeights = dict()
-        allWeights['w'] = tf.Variable(tf.random_normal([self.numVis, self.numHid], stddev=0.01, dtype=tf.float32),name=self.names[0])
-        allWeights['vb'] = tf.Variable(tf.zeros([self.numVis], dtype=tf.float32), name=self.names[1])
-        allWeights['hb'] = tf.Variable(tf.random_uniform([self.numHid], dtype=tf.float32), name=self.names[2])
+        allWeights["w"] = tf.Variable(tf.random_normal([self.numVis, self.numHid], stddev=0.01, dtype=tf.float32),name=self.names[0])
+        allWeights["vb"] = tf.Variable(tf.zeros([self.numVis], dtype=tf.float32), name=self.names[1])
+        allWeights["hb"] = tf.Variable(tf.zeros([self.numHid], dtype=tf.float32), name=self.names[2])
+        allWeights["dW"] = tf.Variable(tf.zeros([self.numVis, self.numHid], dtype=tf.float32), name=self.names[3])
+        allWeights["dVb"] = tf.Variable(tf.zeros([self.numVis], dtype=tf.float32), name=self.names[4])
+        allWeights["dHb"] = tf.Variable(tf.zeros([self.numHid], dtype=tf.float32), name=self.names[5])
         return allWeights
 
     def transform(self, x):
@@ -98,11 +115,49 @@ class RBM:
     def sampleProb(self, probs):
         return tf.nn.relu(tf.sign(probs - tf.random_uniform(tf.shape(probs))))
 
+    def sampleThreshold(self, probs, cutoff):
+        return tf.nn.relu(tf.sign(probs - cutoff))
+
     def fit(self, x, epoch):
-        # Train
         for _ in range(epoch):
-            tmpError, self.wShow = self.sess.run([self.errSum, self.w], feed_dict={self.x: x})
-            print self.wShow
+            summary, tmp1, tmp2 = self.sess.run([self.merge, self.errSum, self.updateAll], feed_dict={self.x: x})
+            self.write.add_summary(summary, _)
+
+    def predictH(self, x):
+        return self.sess.run(self.hSample, {self.x: x})
+
+    def predictV(self, x):
+        return self.sess.run(self.vSample, {self.x: x})
+
+    def predictVWithThreshold(self, x, cutoff):
+        vSampleThreshold = self.sampleThreshold(self.vSample, cutoff)
+        return self.sess.run(vSampleThreshold, {self.x: x})
+
+    def getWeight(self):
+        return self.sess.run(self.w)
+
+    def restoreWeights(self, path):
+        saver = tf.train.Saver({self.names[0]: self.weights["w"],
+                                self.names[1]: self.weights["vb"],
+                                self.names[2]: self.weights["hb"],
+                                self.names[3]: self.weights["dW"],
+                                self.names[4]: self.weights["dVb"],
+                                self.names[5]: self.weights["dHb"]})
+        saver.restore(self.sess, path)
+        self.sess.run([self.w.assign(self.weights["w"]), self.vb.assign(self.weights["vb"]), self.hb.assign(self.weights["hb"]),
+                       self.saveDW.assign(self.weights["dW"]), self.saveDVb.assign(self.weights["dVb"]), self.saveDHb.assign(self.weights["dHb"])])
+
+
+    def saveWeights(self, path):
+        self.sess.run([self.weights["w"].assign(self.w), self.weights["vb"].assign(self.vb), self.weights["hb"].assign(self.hb),
+                       self.weights["dW"].assign(self.saveDW), self.weights["dVb"].assign(self.saveDVb), self.weights["dHb"].assign(self.saveDHb)])
+        saver = tf.train.Saver({self.names[0]: self.weights["w"],
+                                self.names[1]: self.weights["vb"],
+                                self.names[2]: self.weights["hb"],
+                                self.names[3]: self.weights["dW"],
+                                self.names[4]: self.weights["dVb"],
+                                self.names[5]: self.weights["dHb"]})
+        saver.save(self.sess, path)
 
 if __name__ == '__main__':
 
@@ -114,5 +169,13 @@ if __name__ == '__main__':
                      [0, 0, 1, 1, 0, 0],
                      [0, 0, 1, 1, 1, 0]])
 
-    rbm = RBM (6, 2, ["w1", "vb1", "hb1"])
-    rbm.fit(data, 1)
+    rbm = RBM (6, 2, ["w", "vb", "hb", "dW", "dVb", "dHb"], "./logs")
+    np.set_printoptions(suppress=True)
+    rbm.fit(data, 100)
+    rbm.saveWeights("./model.ckpt")
+    # rbm.restoreWeights("./model.ckpt")
+    print rbm.predictH(np.array([[0, 0, 0, 1, 1, 0]]))
+    print rbm.predictV(np.array([[0, 0, 0, 1, 1, 0]]))
+
+    # [[0.00000313  0.99999726]]
+    # [[0.0000861   0.00022533  0.99443108  0.99975079  0.9840337   0.00442834]]
